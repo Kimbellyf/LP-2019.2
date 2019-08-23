cb52135805ccb21426552def953d9b81d6893091
Kimbelly Ferraz <kimbelly_emanuelle@hotmail.com>
2019-08-23 06:01:45 -0300

    Update estruturadocarrooumoto.py

--- entrega01/estruturadocarrooumoto.py
+++ entrega01/estruturadocarrooumoto.py
@@ -1,11 +1,14 @@
-dicVeiculo={"carro":"4","moto":"2"}
+
+def numRodasVeiculo(veiculo):
+    dicVeiculo={"carro":"4","moto":"2"}
+    return dicVeiculo[veiculo]
 
 while True:
     try:
         veiculo=input("Digite 'carro' ou 'moto' e saiba quantas rodas ele tem e digite 0 para parar a aplicação")
         if(veiculo=='0'):
             break
-        resposta=dicVeiculo[veiculo]
+        resposta=numRodasVeiculo(veiculo)
         print(resposta)
     except:
         print("Entrada incorreta")
